15f270e1b78af2b8950b88605414a8edb756de9f
Blillo90 <ppherrero90@gmail.com>
2025-01-11 11:21:50 +0100

    Added css to App

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,7 +1,24 @@
+import './App.css'
+
 export function App () {
     return (
-        <div>
-            <h1>Twitter Card</h1>
-        </div>
+        <article className="pp-tw-followCard">
+            <header className="pp-tw-followCard-header">
+                <img 
+                className="pp-tw-followCard-avatar"
+                src="https://unavatar.io/blillo90/" 
+                alt="blillo90" />
+                <div className="pp-tw-followCard-div">
+                    <strong>Pablo Pérez</strong>
+                    <span className="pp-tw-followCard-span">@blillo90</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className="pp-tw-followCard-button">
+                    Seguir
+                </button>
+            </aside>
+        </article>
     )
 }
--- App/src/main.jsx
+++ App/src/main.jsx
@@ -1,6 +1,7 @@
 import {App} from './App.jsx' 
 import React from 'React'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
